f620899d9e29263aaea800fc159e9b242ed443b3
LeoZorzoli <zorzolileonel@gmail.com>
2020-09-18 16:36:16 -0300

    'part4.4'

---
+++ part4/utils/list_helper.js
@@ -0,0 +1,28 @@
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  const reducer = (sum, blog) => {
+    return sum + blog.likes
+  }
+
+  return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  const reducer = (rest, blog) => {
+    if (blog.likes > rest.likes){
+      return { title: blog.title, author: blog.author, likes: blog.likes }
+    }
+
+    return rest
+  }
+
+  return blogs.length === 0 ? null : blogs.reduce(reducer, blogs[0])
+}
+
+module.exports = {
+  dummy, totalLikes, favoriteBlog
+}
+
