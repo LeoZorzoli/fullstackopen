3a945ee3aa7e8a1a78fce39d37b14c0b17110d58
LeoZorzoli <zorzolileonel@gmail.com>
2020-09-19 15:53:36 -0300

    '4.9'

--- part4/models/blog.js
+++ part4/models/blog.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose')
 mongoose.set('useFindAndModify', false)
 
 const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-  })
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
 
 blogSchema.set('toJSON', {
   transform: (document, returnedObject) => {
--- part4/test/blogs_api.test.js
+++ part4/test/blogs_api.test.js
@@ -4,12 +4,33 @@ const app = require('../app')
 
 const api = supertest(app)
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+
+describe ('getting blogs', () => {
+  test('blogs are returned as json', async () => {
+    await api.get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('there are one blog', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(1)
+  })
+
+  test('blogs contain id', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body[0].id).toBeDefined()
+  })
 })
+
+describe('creating blogs', () => {
+  test('blog added', async () => {
+
+  })
+})
+
 
 afterAll(() => {
   mongoose.connection.close()
