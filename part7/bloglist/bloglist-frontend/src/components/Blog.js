ee9203dce7e6fddf5ec0d00df60c424571e00ac8
LeoZorzoli <zorzolileonel@gmail.com>
2020-11-25 20:39:04 -0300

    finish 7.15

--- part7/bloglist/bloglist-frontend/src/components/Blog.js
+++ part7/bloglist/bloglist-frontend/src/components/Blog.js
@@ -1,13 +1,6 @@
-import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { deleteBlog, likeBlog } from '../reducers/blogReducer'
+import React from 'react'
 
 const Blog = ({ blog }) => {
-  const [showDetails, setShowDetails] = useState(false)
-
-  const toggleShowDetails = () => {
-    setShowDetails(state => !state)
-  }
 
   const blogStyle = {
     paddingTop: 10,
@@ -18,52 +11,9 @@ const Blog = ({ blog }) => {
     marginBottom: 5
   }
 
-  const user = useSelector(state => state.login)
-
-  const dispatch = useDispatch()
-
-  const removeBlog = async (id) => {
-    dispatch(deleteBlog(id))
-  }
-
-  const addLike = async (blog) => {
-    const { author, url, title } = blog
-    const updatedBlog = {
-      user: blog.user?.id || blog.user,
-      likes: blog.likes + 1,
-      title,
-      author,
-      url,
-    }
-    dispatch(likeBlog(blog.id, updatedBlog))
-  }
-
-  if(showDetails){
-    return(
-      <div className='blog' style={blogStyle}>
-        <span className='title'>title: {blog.title}</span>
-        <button onClick={toggleShowDetails}>Hide</button>
-        <div>
-          user: {blog.user.name}
-        </div>
-        <div>
-          url: {blog.url}
-        </div>
-        <div>
-          Likes: {blog.likes}
-          <button onClick={() => addLike(blog)}>Like</button>
-        </div>
-        <div>
-          author: {blog.author}
-        </div>
-        <button onClick={() => removeBlog(blog.id)}>Remove</button>
-      </div>
-    )
-  }
-
   return(
     <div className='blog' style={blogStyle}>
-      {blog.title} {blog.author} <button onClick={toggleShowDetails}>View</button>
+      {blog.title} 
     </div>
   )
 }
--- part7/bloglist/bloglist-frontend/src/components/pages/Blog.js
+++ part7/bloglist/bloglist-frontend/src/components/pages/Blog.js
@@ -1,12 +1,33 @@
 import React from 'react'
-import { useRouteMatch  } from 'react-router-dom' 
-import { useSelector } from 'react-redux'
+import { useRouteMatch, useHistory } from 'react-router-dom' 
+import { useSelector, useDispatch } from 'react-redux'
+import { deleteBlog, likeBlog } from '../../reducers/blogReducer'
 
 const User = () => {
 
     const blogs = useSelector(state => state.blogs)
     const match = useRouteMatch('/blogs/:id')
     const blog = match ? blogs.find(blog => blog.id === match.params.id) : null
+
+    const dispatch = useDispatch()
+    const history = useHistory()
+
+    const removeBlog = async (id) => {
+        dispatch(deleteBlog(id))
+        history.push("/")
+    }
+
+    const addLike = async (blog) => {
+        const { author, url, title } = blog
+        const updatedBlog = {
+          user: blog.user?.id || blog.user,
+          likes: blog.likes + 1,
+          title,
+          author,
+          url,
+        }
+        dispatch(likeBlog(blog.id, updatedBlog))
+    }
     
 
     return(
@@ -15,7 +36,10 @@ const User = () => {
             <div>
                 <p>Author: {blog.author}</p>
                 <p>Url: {blog.url}</p>
+                <p>Likes: {blog.likes}</p>
                 <p>Added by {blog.user.name}</p>
+                <button onClick={() => addLike(blog)}>Like</button>
+                <button onClick={() => removeBlog(blog.id)}>Delete</button>
             </div>
             
         </div>
